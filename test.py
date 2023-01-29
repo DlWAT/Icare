2472671050c729d9cf9fd6585eea56640863d7f7
DlWAT <duwat.adrien@gmail.com>
2023-01-29 02:03:41 +0100

    first tests done

--- test.py
+++ test.py
@@ -1,42 +1,56 @@
 #import pysimpledmx
 #mydmx = pysimpledmx.DMXConnection(3)
-
+import serial.tools.list_ports
 import serial, sys
+import numpy as np
+import random
+import time
+comlist = list(serial.tools.list_ports.comports())
+for port_no, description, address in comlist:
+    if 'USB' in description:
+        MyComPort = port_no
+        print(MyComPort)
 
-START_VAL   = 0x7E
-END_VAL     = 0xE7
+data= np.zeros([512], np.uint8)
+comport=str(port_no)
+try :
+    com = serial.Serial(comport, baudrate = 250000)
+    print('dmx interface connected')
+except:
+    print('connection failed')
+i=0
+g=400
+r=40
+b=0
+tempo=5
+while i<int(255/tempo):
+    
+    time.sleep(0.01*tempo)
+    max=50
+    #r=random.randint(1, max)
+    #g=random.randint(1, max)
+    #b=random.randint(1, max)
 
-COM_BAUD    = 57600
-COM_TIMEOUT = 1
-COM_PORT    = 7
-DMX_SIZE    = 512
+    #b-=tempo
+    #r+=tempo
+    print(r,g,b)
 
-LABELS = {
-         'GET_WIDGET_PARAMETERS' :3,  #unused
-         'SET_WIDGET_PARAMETERS' :4,  #unused
-         'RX_DMX_PACKET'         :5,  #unused
-         'TX_DMX_PACKET'         :6,
-         'TX_RDM_PACKET_REQUEST' :7,  #unused
-         'RX_DMX_ON_CHANGE'      :8,  #unused
-      }
+    data[0] = r
+    data[1] = g
+    data[2] = b
+    data[3] = 20
+    data[4] = 150
+    #dmx_frame[5] = 0
+    start_byte = np.array([0x7E, 0x06, 0x01, 0x02, 0x00], np.uint8)
+    end_byte = np.array([0xE7], np.uint8)
+    num_of_channels=512
 
+    start_byte[2] = (num_of_channels+1) & 0xFF
+    start_byte[3] = ((num_of_channels+1) >> 8) & 0xFF
 
-
-dmx_frame = [0] * DMX_SIZE
-comport=None
-com = serial.Serial(comport, baudrate = COM_BAUD, timeout = COM_TIMEOUT)
-dmx_frame[1-1] = 0
-packet = [
-              START_VAL,
-              LABELS['TX_DMX_PACKET'],
-              len(dmx_frame) & 0xFF,
-              (len(dmx_frame) >> 8) & 0xFF,
-    ]
-packet += dmx_frame
-packet.append(END_VAL)
-
-packet = map(chr, packet)
-com.write(''.join(packet))
+    data_out = np.concatenate((start_byte, data, end_byte)).tobytes()
+    i+=1
+    com.write(data_out)
 
 def main_test(mydmx):
 
