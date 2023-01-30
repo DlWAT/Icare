df7ff531dff24084e734bac8d93745a04df0506f
DlWAT <duwat.adrien@gmail.com>
2023-01-30 15:08:13 +0100

    creation of the icare module and cleaning

---
+++ pydmxIcare.py
@@ -0,0 +1,47 @@
+from gettext import npgettext
+import serial.tools.list_ports
+import serial, sys
+import numpy as np
+import warnings
+
+
+class DMX_Icare(object):
+
+    def __init__(self) -> None:
+        self.start_byte = np.array([0x7E, 0x06, 0x01, 0x02, 0x00], np.uint8)
+        self.end_byte = np.array([0xE7], np.uint8)
+        num_of_channels=512
+        self.start_byte[2] = (num_of_channels+1) & 0xFF
+        self.start_byte[3] = ((num_of_channels+1) >> 8) & 0xFF
+        self.data= np.zeros([512], np.uint8)
+        
+
+    def connection(self):
+        comlist = list(serial.tools.list_ports.comports())
+        if not comlist:
+            warnings.warn("No COM port available")
+        try :    
+            for port_no, description, address in comlist:
+                if 'USB' in description:
+                    comport=str(port_no)
+            self.com = serial.Serial(comport, baudrate = 250000)
+            print('DMX interface connected')
+        except Exception as e:
+            print('Error : Connection failed')
+
+
+    def set_channel(self,num_channel,value):
+        if type(num_channel) is not int:
+            raise TypeError(str(num_channel)+" Must be int")
+        if type(value) is not int:
+            raise TypeError(str(num_channel)+" Must be int")
+        if num_channel >511 :
+            raise ValueError(num_channel+" Must be less than 512")
+        if value >255 :
+            raise ValueError(num_channel+" Must be less than 255")
+        self.data[num_channel] = value
+
+
+    def send_data(self):
+        data_out = np.concatenate((self.start_byte, self.data, self.end_byte)).tobytes()
+        self.com.write(data_out)
