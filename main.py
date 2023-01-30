43488cbc11ebb02f752d7af44c2d00660dc6d6b4
DlWAT <duwat.adrien@gmail.com>
2023-01-30 15:46:36 +0100

    new module, with class for each light system

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import pydmxIcare
-
+import pydmxlight
 mydmx= pydmxIcare.DMX_Icare()
-mydmx.connection()
+par_led1=pydmxlight.Par_Led_615_AFX(5,0)
+
+print(mydmx.data[0:5])
+
 mydmx.set_channel(1,255)
+print(mydmx.data[0:5])
+
+par_led1.set_channel(2,125)
+par_led1.set_channel(3,18)
+mydmx.set_data(par_led1.data,5,5)
+print(mydmx.data[0:10])
