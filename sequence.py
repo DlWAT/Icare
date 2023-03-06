54211cd4b2426dec3973c0e867ce5ffcfbfebc7f
DlWAT <duwat.adrien@gmail.com>
2023-03-06 09:42:10 +0100

    add treeviewer

--- sequence.py
+++ sequence.py
@@ -26,10 +26,10 @@ class SequenceTab(ttk.Frame):
     def creer_widgets(self):
             wrapper1 = tk.LabelFrame(self,text="Sequence")
             wrapper1.pack(fill="both",expand="yes",padx=20,pady=10)
-            trv=ttk.Treeview(wrapper1,columns=(1,2,3,4))
+            self.trv=ttk.Treeview(wrapper1,columns=(1,2,3,4))
             style = ttk.Style(trv)
             style.configure('Trerview',rowheight=30,height="5")
-            trv.pack(expand=True, fill='both')
+            self.trv.pack(expand=True, fill='both')
             
             
             xscroll=ttk.Scrollbar(wrapper1,orient="horizontal",command=trv.xview)
@@ -37,13 +37,33 @@ class SequenceTab(ttk.Frame):
             
             wrapper2 = tk.LabelFrame(self,text="Command")
             wrapper2.pack(fill="both",expand="yes",padx=20,pady=10)
-            titles={'Id': [1,2,3,4,5, 6, 7, 8, 9,10,11,12,13], 'Names':['Tom', 'Rob', 'Tim', 'Jim', 'Kim', 'Kim', 'Kim', 'Kim', 'Kim', 'Jim', 'Jim', 'Jim', 'Jim'], 'Column': [1,2,3,4,5, 6, 7, 8, 9,10,11,12,13]}
-            trv["columns"] = list(x for x in range(len(list(titles.keys()))))
-            trv['show'] = 'headings'
+            titles={'ParLED1': [1,2,3,4,5, 6, 7, 8, 9,10,11,12,13], 'Names':['Tom','', 'Tim', 'Jim', 'Kim', 'Kim', 'Kim', 'Kim', 'Kim', 'Jim', 'Jim', 'Jim', 'Jim'], 'Column': [1,2,3,4,5, 6, 7, 8, 9,10,11,12,13]}
+            self.trv["columns"] = list(x for x in range(len(list(titles.keys()))))
+            self.trv['show'] = 'headings'
             for x, y in enumerate(titles.keys()):
-                  trv.column(x, minwidth=20, stretch=True,  anchor='c')
-                  trv.heading(x, text=y)
+                  self.trv.column(x, minwidth=20, stretch=True,  anchor='c')
+                  self.trv.heading(x, text=y)
 
             for args in zip(*list(titles.values())):
-                  trv.insert("", 'end', values =args) 
-          
+                  self.trv.insert("", 'end', values =args) 
+            self.bouton_blackout= tk.Button(wrapper2, text="Blackout")#, command = self.blackout)
+            self.bouton_blackout.grid(column=0, row=0)
+            
+            self.bouton_rand= tk.Button(wrapper2, text="Random")#, command = self.rand_light)
+            self.bouton_rand.grid(column=0, row=1)
+            
+            self.bouton_strobe= tk.Button(wrapper2, text="Strobe")#, command = self.strobe)
+            self.bouton_strobe.grid(column=0, row=2)
+            
+            self.bouton_rand= tk.Button(wrapper2, text="Connection")#, command = self.connection)
+            self.bouton_rand.grid(column=1, row=0)
+            
+            self.bouton_co= tk.Button(wrapper2, text="Connection")#, command = self.connection)
+            self.bouton_co.grid(column=1, row=1)
+            
+            self.bouton_color= tk.Button(wrapper2, text="Color")#, command = self.ask_color)
+            self.bouton_color.grid(column=1, row=2)
+        
+    def add_item(self):
+        self.trv.insert("", "end", values=("", "bar"))
+            
